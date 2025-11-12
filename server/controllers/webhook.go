f47233c1655a6b02910e65703bf3087ed76e81c2
rfking <xperiarfsonn@gmail.com>
2025-11-12 15:39:32 -0300

    Implement webhook controller with add and delete functions

---
+++ server/controllers/webhook.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+    "github.com/labstack/echo/v4"
+)
+
+var webhooks = make(map[string]string) // Simples mapexemplo, pode ser BD depois!
+
+func AddWebhook(ctx echo.Context) error {
+    instanceId := ctx.Param("instance")
+    type Request struct {
+        URL string `json:"url"`
+    }
+    var req Request
+    if err := ctx.Bind(&req); err != nil {
+        return ctx.JSON(400, echo.Map{"error": "request inválido"})
+    }
+    webhooks[instanceId] = req.URL
+    return ctx.JSON(200, echo.Map{"message": "Webhook adicionado!", "url": req.URL})
+}
+
+func DeleteWebhook(ctx echo.Context) error {
+    instanceId := ctx.Param("instance")
+    delete(webhooks, instanceId)
+    return ctx.JSON(200, echo.Map{"message": "Webhook removido!"})
+}

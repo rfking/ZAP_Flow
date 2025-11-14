f1f5786d9b113e754e63f1eb42a92f5b36fef33d
rfking <xperiarfsonn@gmail.com>
2025-11-14 13:28:31 -0300

    Create webhook.go
    Adicionar caminho e rotas para Webhook

---
+++ server/routes/webhook.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+func WebhookHandler(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Recebido %s no webhook", r.Method)
+    body, _ := ioutil.ReadAll(r.Body)
+    log.Println("Headers:", r.Header)
+    log.Println("Payload:", string(body))
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"status": "success"}`))
+}

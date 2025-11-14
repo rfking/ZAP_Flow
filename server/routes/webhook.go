7a76a1f44955709c0fbff03a9cd188cb9ba85b4b
rfking <xperiarfsonn@gmail.com>
2025-11-14 14:13:44 -0300

    Update webhook.go

--- server/routes/webhook.go
+++ server/routes/webhook.go
@@ -1,11 +1,16 @@
-package main
+package routes
 
 import (
+    "io/ioutil"
+    "log"
     "net/http"
-    "github.com/verbeux-ai/whatsmiau/server/routes"
 )
 
-func main() {
-    http.HandleFunc("/webhook", routes.WebhookHandler)
-    http.ListenAndServe(":8081", nil)
+func WebhookHandler(w http.ResponseWriter, r *http.Request) {
+    log.Printf("Recebido %s no webhook", r.Method)
+    body, _ := ioutil.ReadAll(r.Body)
+    log.Println("Headers:", r.Header)
+    log.Println("Payload:", string(body))
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"status": "success"}`))
 }

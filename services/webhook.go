e341bd3936c1fe37cb2543717951f9be04565918
rfking <xperiarfsonn@gmail.com>
2025-11-14 17:04:53 -0300

    Update webhook.go

--- services/webhook.go
+++ services/webhook.go
@@ -1,26 +1,11 @@
 package services
 
-import "sync"
-
-var (
-	webhookURL string
-	lock       sync.RWMutex
-)
+var webhookURL string
 
 func SetWebhook(url string) {
-	lock.Lock()
 	webhookURL = url
-	lock.Unlock()
 }
 
 func GetWebhook() string {
-	lock.RLock()
-	defer lock.RUnlock()
 	return webhookURL
-}
-
-func DeleteWebhook() {
-	lock.Lock()
-	webhookURL = ""
-	lock.Unlock()
 }
